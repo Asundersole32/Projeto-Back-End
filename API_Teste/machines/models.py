1fd5f2b8525a5e214f3914de9c9cdc27089d1c6d
Erik Casa <erikviana32@gmail.com>
2026-06-30 01:30:42 -0400

    adicionando os modelos das maquinas

--- API_Teste/machines/models.py
+++ API_Teste/machines/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Line(models.Model):
+    machine_qtd=models.IntegerField(default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class Machine(models.Model):
+    machineName=models.CharField(max_length=100)
+    machineType=models.CharField(max_length=100)
+    company=models.CharField(max_length=100)
+    line=models.ForeignKey(Line, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+
--- API_Teste/machines/views.py
+++ API_Teste/machines/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
+from rest_framework import generics, status
 
 # Create your views here.
