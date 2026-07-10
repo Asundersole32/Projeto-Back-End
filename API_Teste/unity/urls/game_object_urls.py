aacef5ea70e1b496bbd932e68d0eb6843b2993ba
Erik Felipe Viana Rocha <erik.rocha@conecthus.org.br>
2026-07-10 10:42:10 -0400

    endpoints de retorno dos game objects completos

--- API_Teste/unity/urls/game_object_urls.py
+++ API_Teste/unity/urls/game_object_urls.py
@@ -5,12 +5,16 @@ from unity.viewsets.game_object_view.delete_game_object_view import DeleteGameOb
 from unity.viewsets.game_object_view.get_game_object_view import GetGameObjectView
 from unity.viewsets.game_object_view.list_game_objects_view import ListGameObjectView
 from unity.viewsets.game_object_view.update_game_object_view import UpdateGameObjectView
+from unity.viewsets.game_object_view.get_complete_game_object_view import GetCompleteGameObjectView
+from unity.viewsets.game_object_view.list_complete_game_object_view import ListCompleteGameObjectsView
 
 
 urlpatterns = [
     path('game_object/register/', CreateGameObjectView.as_view(), name='game_object_create_route'),
     path('game_object/list/', ListGameObjectView.as_view(), name='game_object_list_route'),
-    path('game_object/update/<int:game_object_id>/', UpdateGameObjectView.as_view(), name='game_object_update_route'),
-    path('game_object/delete/<int:game_object_id>/', DeleteGameObjectView.as_view(), name='game_object_delete_route'),
-    path('game_object/<int:game_object_id>/', GetGameObjectView.as_view(), name='game_object_get_route'),
+    path('game_object/list/complete/', ListCompleteGameObjectsView.as_view(), name='list_complete_game_object_route'),
+    path('game_object/update/<str:game_object_id>/', UpdateGameObjectView.as_view(), name='game_object_update_route'),
+    path('game_object/delete/<str:game_object_id>/', DeleteGameObjectView.as_view(), name='game_object_delete_route'),
+    path('game_object/complete/<str:game_object_id>/', GetCompleteGameObjectView.as_view(), name='get_complete_game_object_view'),
+    path('game_object/<str:game_object_id>/', GetGameObjectView.as_view(), name='game_object_get_route'),
 ]
---
+++ API_Teste/unity/viewsets/game_object_view/get_complete_game_object_view.py
@@ -0,0 +1,46 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from unity.models import GameObject, Metadata, GameObjectTransform, GameObjectScale, GameObjectPosition, GameObjectRotation
+
+from unity.serializers.game_object_position_serializer import GameObjectPositionSerializer
+from unity.serializers.game_object_rotation_serializer import GameObjectRotationSerializer
+from unity.serializers.game_object_scale_serializer import GameObjectScaleSerializer
+from unity.serializers.game_object_serializer import GameObjectSerializer
+from unity.serializers.game_object_transform_serializer import GameObjectTransformSerializer
+from unity.serializers.metadata_serializer import MetadataSerializer
+
+
+class GetCompleteGameObjectView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, game_object_id=None):
+        try:
+            return_info = {}
+            
+            game_object = GameObject.objects.get(pk=game_object_id)
+            metadata = Metadata.objects.filter(gameobject=game_object).last()
+            game_object_transform = GameObjectTransform.objects.filter(game_object=game_object).last()
+            game_object_position = GameObjectPosition.objects.filter(game_object_transform=game_object_transform).last()
+            game_object_rotation = GameObjectRotation.objects.filter(game_object_transform=game_object_transform).last()
+            game_object_scale = GameObjectScale.objects.filter(game_object_transform=game_object_transform).last()
+
+            game_object_serializer = GameObjectSerializer(game_object)
+            metadata_serializer = MetadataSerializer(metadata)
+            game_object_transform_serializer = GameObjectTransformSerializer(game_object_transform)
+            game_object_position_serializer = GameObjectPositionSerializer(game_object_position)
+            game_object_scale_serializer = GameObjectScaleSerializer(game_object_scale)
+            game_object_rotation_serializer = GameObjectRotationSerializer(game_object_rotation)
+
+            return_info['metadata'] = metadata_serializer.data
+            return_info['game_object'] = game_object_serializer.data
+            return_info['game_object']['transform'] = game_object_transform_serializer.data
+            return_info['game_object']['transform']['position'] = game_object_position_serializer.data
+            return_info['game_object']['transform']['rotation'] = game_object_rotation_serializer.data
+            return_info['game_object']['transform']['scale'] = game_object_scale_serializer.data
+
+            return Response(return_info, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'message': str(error)}, status=status.HTTP_400_BAD_REQUEST)
